8210cab4d67c3b0a04bd501231337380f2dbf719
Leandro Abreu <leandro.sabreu@uvvnet.com.bru>
2023-11-19 22:26:16 -0300

    bugfix(Deploy): Url

--- frontend/src/environment/backend.js
+++ frontend/src/environment/backend.js
@@ -2,7 +2,7 @@ const backend = () => {
     let link;
 
     //ip local
-    link = "http://stocksolutionapp.azurewebsites.net/api"
+    link = "https://stocksolutionapp.azurewebsites.net/api"
     // link = "http://192.168.0.165:3001"
     // link = "http://192.168.15.11:3001"
 
